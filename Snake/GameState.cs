2bc1d4294ed623aa6fd3b52db082aeba81c3929d
andreivarga36 <andreivarga36@yahoo.com>
2024-06-07 19:05:37 +0300

    UI was configured

---
+++ Snake/GameState.cs
@@ -0,0 +1,147 @@
+﻿namespace Snake
+{
+    public class GameState
+    {
+        private readonly LinkedList<Position> snakePositions = new();
+        private readonly Random random = new();
+
+        public GameState(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            Grid = new GridValue[rows, columns];
+            Dir = Direction.Right;
+
+            AddSnake();
+            AddFood();
+        }
+
+        public int Rows { get; }
+
+        public int Columns { get; }
+
+        public GridValue[,] Grid { get; }
+
+        public Direction Dir { get; private set; }
+
+        public int Score { get; private set; }
+
+        public bool GameOver { get; private set; }
+
+        public Position HeadPosition()
+        {
+            return snakePositions.First!.Value;
+        }
+
+        public Position TailPosition()
+        {
+            return snakePositions.Last!.Value;
+        }
+
+        public IEnumerable<Position> SnakePositions()
+        {
+            return snakePositions;
+        }
+
+        public void ChangeDirection(Direction dir)
+        {
+            Dir = dir;
+        }
+
+        public void Move()
+        {
+            Position newHeadPos = HeadPosition().Translate(Dir);
+            GridValue hit = WillHit(newHeadPos);
+
+            if (hit == GridValue.Out || hit == GridValue.Snake)
+            {
+                GameOver = true;
+            }
+            else if (hit == GridValue.Empty)
+            {
+                RemoveTail();
+                AddHead(newHeadPos);
+            }
+            else if (hit == GridValue.Food)
+            {
+                AddHead(newHeadPos);
+                Score++;
+                AddFood();
+            }
+        }
+
+        private void AddSnake()
+        {
+            int middleRow = Rows / 2;
+
+            for (int c = 0; c <= Columns; c++)
+            {
+                Grid[middleRow, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(middleRow, c));
+            }
+        }
+
+        private IEnumerable<Position> EmptyPositions()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+
+        private void AddFood()
+        {
+            List<Position> emptyPositions = new List<Position>(EmptyPositions());
+
+            if (emptyPositions.Count == 0)
+            {
+                return;
+            }
+
+            Position pos = emptyPositions[random.Next(emptyPositions.Count)];
+            Grid[pos.Row, pos.Column] = GridValue.Food;
+        }
+
+        private void AddHead(Position pos)
+        {
+            snakePositions.AddFirst(pos);
+            Grid[pos.Row, pos.Column] = GridValue.Snake;
+        }
+
+        private void RemoveTail()
+        {
+            Position tail = snakePositions.Last!.Value;
+            Grid[tail.Row, tail.Column] = GridValue.Empty;
+            snakePositions.RemoveLast();
+        }
+
+        private bool IsOut(Position pos)
+        {
+            return pos.Row < 0 ||
+                pos.Row >= Rows ||
+                pos.Column < 0 ||
+                pos.Column >= Columns;
+        }
+
+        private GridValue WillHit(Position newHeadPos)
+        {
+            if (IsOut(newHeadPos))
+            {
+                return GridValue.Out;
+            }
+
+            if (newHeadPos == TailPosition())
+            {
+                return GridValue.Empty;
+            }
+
+            return Grid[newHeadPos.Row, newHeadPos.Column];
+        }
+    }
+}
--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Snake
 {
@@ -16,9 +8,37 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int rows = 15;
+        private readonly int columns = 15;
+        private readonly Image[,] gridImages;
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetupGrid();
+        }
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, columns];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = columns;
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    Image image = new Image()
+                    {
+                        Source = Images.Empty
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            return images;
         }
     }
 }
